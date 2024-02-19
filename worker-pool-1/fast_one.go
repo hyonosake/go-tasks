123421d8ac8a4442b08b3c51b3a2cf383710ff67
Roman Dubenets <rdubenets@wimark.com>
2024-02-20 00:58:43 +0300

    Add: go func

--- worker-pool-1/fast_one.go
+++ worker-pool-1/fast_one.go
@@ -1,6 +1,14 @@
 package worker_pool_1
 
-func fastLoad(_ []Request) map[string]Response {
+func fastLoad(requests []Request) map[string]Response {
 	/* тут твой кодик */
+	results := make(map[string]Response)
+	for _, req := range requests {
+		reqs := req
+		go func() {
+			results[reqs.uuid] = looooooad(reqs)
+		}()
+	}
 	return nil
+
 }
